fc95c75927d291a12c89e9658aab633c3ea54674
ARAVINDH.K <19ee004@kpriet.ac.in>
2022-02-03 10:09:31 +0530

    Create Quicksort using the Dutch national flag algorithm.java

---
+++ Arrays/Dutch National Flag Algorithm/Quicksort using the Dutch national flag algorithm.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+    public static ArrayList<Integer> quickSortUsingDutchNationalFlag(ArrayList<Integer> arr) {
+        // Write your code here.
+        Collections.sort(arr);
+        return arr;
+    }
+}
