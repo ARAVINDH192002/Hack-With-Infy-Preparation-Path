81c9b7d43e3d6fd560ffd4a0603a937017557a0c
ARAVINDH.K <19ee004@kpriet.ac.in>
2022-02-03 10:05:13 +0530

    Create Sort 0 1 2.java

---
+++ Arrays/Dutch National Flag Algorithm/Sort 0 1 2.java
@@ -0,0 +1,10 @@
+//Approch 1
+import java.util.Arrays;
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+        //Write your code here       
+        Arrays.sort(arr);
+    }
+}
